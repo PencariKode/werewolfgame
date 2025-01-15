423bb24d1e53ad4818815d81c19abe9ff01aa370
PencariKode <Pencaricode@gmail.com>
2025-01-16 00:13:48 +0700

    update: Moving from css file to config file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -26,5 +26,35 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          /* Untuk menyembunyikan scrollbar */
+          '-ms-overflow-style': 'none', /* IE */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none', /* Chrome, Safari */
+        },
+        '.link-underline': {
+          'border-bottom-width': '0',
+          'background-image': 'linear-gradient(transparent, transparent), linear-gradient(#fff, #fff)',
+          'background-size': '0 2px',
+          'background-position': '0 100%',
+          'background-repeat': 'no-repeat',
+          'transition': 'background-size .3s ease-in-out',
+        },
+        
+        '.link-underline-black': {
+          'background-image': 'linear-gradient(transparent, transparent), linear-gradient(var(--tw-underline-color), var(--tw-underline-color))',
+        },
+        
+        '.link-underline:hover': {
+          'background-size': '100% 2px',
+          'background-position': '0 100%',
+        }
+      });
+    },
+  ],
 };
