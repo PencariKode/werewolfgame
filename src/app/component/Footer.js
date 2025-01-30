afd48ec5b025b83ddf44d7d3474ebadb685398ac
PencariKode <Pencaricode@gmail.com>
2025-01-30 13:07:22 +0700

    add: disabled prefetch for Footer

--- src/app/component/Footer.js
+++ src/app/component/Footer.js
@@ -31,9 +31,9 @@ export default function Footer() {
     return (
         <footer className="overflow-clip bg-dark-primary min-w-full !max-w-full min-h-[10] flex flex-col md:flex-row md:gap-10 md:pl-10 py-7 px-6 lg:pl-16">
             <section className="mincomp flex flex-col md:max-w-[35%] md:min-w-[35%] md:justify-center ">
-                <Link href="/" className={`${frijole.className} text-3xl`}>WOLFMAN</Link>
+                <Link href="/" prefetch={false} className={`${frijole.className} text-3xl`}>WOLFMAN</Link>
                 <p className="text-sm pl-1">© 2024 WOLFMAN. All rights reserved.</p>
-                <p className="text-sm pt-1.5 pl-1">Made with <i className="fa-solid fa-heart"></i> By <Link href="//github.com/PencariKode" className="font-bold link-underline link-underline-black">Panji Depari</Link></p>
+                <p className="text-sm pt-1.5 pl-1">Made with <i className="fa-solid fa-heart"></i> By <Link href="//github.com/PencariKode" prefetch={false} className="font-bold link-underline link-underline-black">Panji Depari</Link></p>
             </section>
             <section className="mincomp flex flex-col mt-10 md:max-w-[calc(65%-2.5rem)] md:min-w-[calc(65%-2.5rem)]">
                 <div className="mincomp flex flex-col *:*:*:max-w-fit lg:flex-row gap-4 lg:gap-0">
@@ -42,7 +42,7 @@ export default function Footer() {
                             {link.map((item, index) => (
                                 <div key={index} className="mincomp flex flex-col gap-1  xs:max-w-[50%] xs:min-w-[50%]">
                                     {item.map((subitem, index) => (
-                                        <Link key={index} href={subitem.link} className="text-sm link-underline link-underline-black">{subitem.nama}</Link>
+                                        <Link key={index} prefetch={false} href={subitem.link} className="text-sm link-underline link-underline-black">{subitem.nama}</Link>
                                     ))}
                                 </div>
                             ))}
